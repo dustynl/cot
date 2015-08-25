f4d2a7b9a1a26b7f2b13ffedf118dba719602694
Dustyn Lightfoot <dustyn.lightfoot@gmail.com>
2015-08-25 18:57:43 +0200

    initial version

---
+++ cot.client.maker.core/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cot.client.maker.core
+{
+    public class Class1
+    {
+    }
+}
---
+++ cot.service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cot.service.WebApiApplication" Language="C#" %>
